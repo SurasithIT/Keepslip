e241f3cf5a383a67376f4861e763f72b21e62d85
Surasith <surasith@Surasiths-MBP.lan>
2020-04-30 13:52:45 +0700

    Fixed 'Read more' button size

--- client/components/receipt/receiptMini.js
+++ client/components/receipt/receiptMini.js
@@ -49,7 +49,7 @@ class ReceiptMini extends Component {
           <Link
             href={{
               pathname: "/receipt",
-              query: { receipt_id: this.props.receipt_id }
+              query: { receipt_id: this.props.receipt_id },
             }}
           >
             <button>Read More</button>
@@ -71,15 +71,14 @@ class ReceiptMini extends Component {
               font-size: 23px;
             }
             button {
-              width: 80px;
-              height: 30px;
+              width: fit-content;
               border-radius: 20px;
               border: 1px solid #ddd;
               cursor: pointer;
               margin: 1em;
               background: #1564bf;
               color: white;
-              // padding: 10px;
+              padding: 5px 10px;
             }
 
             button:hover {
